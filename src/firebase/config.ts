df4229655c81d17c4f0256c70e7ed392ba2ba61c
egaborigen <egaborigen@gmail.com>
2026-04-16 17:27:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-764865538-617c4",
+  "appId": "1:309743445283:web:55ded0c4494593ecb05896",
+  "apiKey": "AIzaSyCby9gap3bOe0h1uKENtZ2e2b8xU4i_ziE",
+  "authDomain": "studio-764865538-617c4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "309743445283"
+};
